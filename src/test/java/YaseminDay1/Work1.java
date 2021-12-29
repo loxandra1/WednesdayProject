2c240db3783dba3818433a52f063f60f264d1088
Yasemin0955 <ymacit0955@gmail.com>
2021-12-29 10:49:30 +0000

    second

--- src/test/java/YaseminDay1/Work1.java
+++ src/test/java/YaseminDay1/Work1.java
@@ -3,6 +3,7 @@ package YaseminDay1;
 public class Work1 {
     public static void main(String[] args) {
         System.out.println("HELLO FROM THE OTHER SIDE");
+        //want to change
     }
 
 }
