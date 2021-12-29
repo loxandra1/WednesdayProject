4abec61f25084be7a4de1402d2fa73ab338b983e
ReyhaneAysan <reyhaneaysan@gmail.com>
2021-12-29 10:49:14 +0000

    Second commit

--- src/test/java/Reyhane/ReyhaneWork.java
+++ src/test/java/Reyhane/ReyhaneWork.java
@@ -4,5 +4,6 @@ public class ReyhaneWork {
     public static void main(String[] args) {
         System.out.println("Hi");
         System.out.println("Ladies");
+        System.out.println("We can do everything");
     }
 }
